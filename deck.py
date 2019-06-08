ed805e19ef525101ea479d2b78ba9184c68cbbce
arjundevn <45147987+arjundevn@users.noreply.github.com>
2019-06-08 20:10:30 +0530

    Add files via upload

---
+++ deck.py
@@ -0,0 +1,28 @@
+import random
+
+def create_deck():
+    '''
+    creates deck
+    '''
+    type = ['Spades', 'Clubs', 'Hearts','Diamonds']
+    deck = []
+    for i in type:
+        for j in range (1,14):
+            if j ==1:
+                deck.append({i:'Ace'})
+            elif j ==11:
+                deck.append({i:'Jack'})
+            elif j ==12:
+                deck.append({i:'Queen'})
+            elif j ==13:
+                deck.append({i:'King'})
+            else:
+                deck.append({i:j})
+    return deck
+
+
+def shuffle_deck(deck):
+    '''
+    shuffles deck
+    '''
+    random.shuffle(deck)
---
+++ main.py
@@ -0,0 +1,7 @@
+import deck
+
+d = deck.create_deck()
+
+deck.shuffle_deck(d)
+
+print(d)
